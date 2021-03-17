706ae00b2fd1ce1b5be02e7f49421a26161633d7
ashikkhandani <mdashikahammed@gmail.com>
2021-03-17 12:42:50 +0100

    ashik added

---
+++ src/components/Ashik.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Ashik = () => {
+  return (
+    <div>
+      <h2>Hello class ! I am Ashik</h2>
+      <img
+        src="https://t4.ftcdn.net/jpg/02/18/89/63/360_F_218896346_x8m03Ft3pb1JRMGkdnsAO2LmMd5BXCCL.jpg"
+        alt="flag"
+        style={{ width: "150px", height: "150px", borderRadius: "50px" }}
+      />
+    </div>
+  );
+};
+
+export default Ashik;
