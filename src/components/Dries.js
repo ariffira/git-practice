da13319da8a1669314a44efa839f37776e55ff75
DriesVerb <verboven.dries@gmail.com>
2021-03-17 11:46:19 +0100

    made a component

---
+++ src/components/Dries.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Dries extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello this is Dries</h1>
+      </div>
+    );
+  }
+}
