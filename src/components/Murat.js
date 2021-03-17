5b3857e3522913f59bdee6b04dca9090cc4fd29d
muratcanoncu <murat.oncu1992@hotmail.com>
2021-03-17 11:50:30 +0100

    Murat Added

---
+++ src/components/Murat.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Murat extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ color: "red" }}>hello im Murat</h1>
+      </div>
+    );
+  }
+}
+
+export default Murat;
