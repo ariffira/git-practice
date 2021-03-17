b5161d2600bf314b5a28fff98cd264ec66c7ef19
ariffira <mac.fira@gmail.com>
2021-03-17 11:47:59 +0100

    conflict from nico

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
-import "./App.css";
-import Arif from "./components/Arif";
-
-
-=======
 import './App.css';
 import Arif from './components/Arif';
 import Jose from './components/Jose';
->>>>>>> c0269b0043d2a51e1b99416f75e74ca95f48d2fb
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Github Team-work practice for fbw41-1</h1>
-<<<<<<< HEAD
-        <Arif />
-        
-=======
         <Arif/>
         <Jose/>
->>>>>>> c0269b0043d2a51e1b99416f75e74ca95f48d2fb
       </header>
     </div>
   );
