de9b4671871ad0f6a69748de61694702128653fb
ArslanRama <arslanrama84@gmail.com>
2021-03-17 11:48:41 +0100

    Aslan

---
+++ src/components/Aslan.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Aslan = () => {
+    return (
+        <div>
+            <h1>Hello Aslan here!</h1>
+            <img src = 'https://images.unsplash.com/photo-1585362028211-dee1aba5fdd3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'/>
+        </div>
+    )
+}
+
+export default Aslan;
