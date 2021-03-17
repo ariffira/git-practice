1a5a0d8ae53e94820fdd772e5eae58be9b9dc795
Nicolas Alvarez <neao002@gmail.com>
2021-03-17 11:08:20 +0100

    adding nicos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-import Arif from './components/Arif';
+import "./App.css";
+import Arif from "./components/Arif";
+
+// nico
+
+import Nico from "./components/nico";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Github Team-work practice for fbw41-1</h1>
-        <Arif/>
+        <Arif />
+        <Nico />
       </header>
     </div>
   );
---
+++ src/components/nico.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Nico extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello there =)</h1>
+      </div>
+    );
+  }
+}
+
+export default Nico;
